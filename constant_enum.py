d0db691cd5c5dc539fd9a3609ebbabcbe03391c6
Aishwarya Shirish Ghirnikar <62713812+AishwaryaGhirnikar@users.noreply.github.com>
2020-07-22 13:55:19 +0530

    Update constant_enum.py

--- constant_enum.py
+++ constant_enum.py
@@ -1,1 +1,7 @@
+from Text_Fw.preprocessing.NLTK_preprocessing import NLTK_preprocessing
+from Text_Fw.preprocessing.spacy_preprocessing import spacy_preprocessing
 
+LibraryBase = {
+    "nltk":NLTK_preprocessing,
+    "spacy":spacy_preprocessing
+}

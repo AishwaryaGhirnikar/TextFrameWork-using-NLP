89e363cdab1fbc7d91854fecfdb958ba010d8913
Aishwarya Shirish Ghirnikar <62713812+AishwaryaGhirnikar@users.noreply.github.com>
2020-07-22 13:54:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from Text_Fw.constant_enum import *
+
+class Main():
+
+
+    def preprocessing_text(self, text, lib_name, funcion_name, param_dict):
+        classname = LibraryBase.get(lib_name,"nltk")
+        class_ref = classname(text)
+        result = getattr(class_ref,funcion_name)(param_dict)
+        return result
+
+if __name__ == "__main__":
+    main = Main()
+    param_dict= {"token type": "word"}
+    print(main.preprocessing_text("my name is Donald Trump","nltk","tokenization",param_dict,))
+    print(main.preprocessing_text("my name is Donald Trump","spacy", "tokenization", param_dict, ))
+
 
